650007ac28680dee06c5d9431678962669369431
Laël <13titirex@pm.me>
2026-02-10 15:18:19 +0100

    add: Colleen asm

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,28 +1,28 @@
 ; Colleen in asm
-global main
-extern printf
+        global main
+        extern printf
+
 SECTION .rodata
-format: DB "SECTION .data%cformat: DB %c%s%c, 0%cSECTION .text%cmain:%c%c%c" , 0
+space: DB "        ", 0
+format: DB "; Colleen in asm%1$c%2$sglobal main%1$c%2$sextern printf%1$c%1$cSECTION .rodata%1$cspace: DB %3$c%2$s%3$c, 0%1$cformat: DB %3$c%4$s%3$c, 0%1$c%1$cSECTION .text%1$cmain:%1$c%2$spush rbp%1$c%2$scall print%1$c%2$spop rbp%1$c%2$sret%1$c%1$cprint:%1$c%2$spush rbp%1$c%2$smov rbp, rsp%1$c%2$smov rdi, format%1$c%2$smov rsi, 10%1$c%2$smov rdx, space%1$c%2$smov rcx, 34%1$c%2$smov r8, format%1c%2$s; call printf%1$c%2$scall printf%1$c%2$sleave%1$c%2$sxor rax, rax%1$c%2$sret%1$c", 0
+
 SECTION .text
 main:
-push rbp
-call print
-pop rbp
-xor rax, rax
-ret
+        push rbp
+        call print
+        pop rbp
+        ret
+
 print:
-push rbp
-mov rbp, rsp
-mov rdi, format
-mov rsi, 10
-mov rdx, 34
-mov rcx, format
-mov r8, 34
-mov r9, 10
-; push the left on the stack, on reverse order!
-push 51
-push 10
-xor al, al
-call printf
-leave
-ret
+        push rbp
+        mov rbp, rsp
+        mov rdi, format
+        mov rsi, 10
+        mov rdx, space
+        mov rcx, 34
+        mov r8, format
+        ; call printf
+        call printf
+        leave
+        xor rax, rax
+        ret
--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -4,13 +4,13 @@
 
 void	print(char *s)
 {
-	printf(s, 10, 10, 92, 92, 34, 92, 10, 10, 9, 10, 10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 9, 34, s, 34, 10, 10, 9, 10, 9, 10, 10);
+	printf(s, 9, 10, 34, 92, s);
 }
 
 int	main(void)
 {
 	// The string below contain the program source code
-	char	*s = "#include <stdio.h>%c%c// help: 10 -> %cn, 9 -> %ct, 34 -> %c, 92 -> '%c'%c%cvoid%cprint(char *s)%c{%c%cprintf(s, 10, 10, 92, 92, 34, 92, 10, 10, 9, 10, 10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 9, 34, s, 34, 10, 10, 9, 10, 9, 10, 10);%c}%c%cint%cmain(void)%c{%c%c// The string below contain the program source code%c%cchar%c*s = %c%s%c;%c%c%cprint(s);%c%creturn (0);%c}%c";
+	char	*s = "#include <stdio.h>%2$c%2$c// help: 10 -> %4$cn, 9 -> %4$ct, 34 -> %3$c, 92 -> '%4$c'%2$c%2$cvoid%1$cprint(char *s)%2$c{%2$c%1$cprintf(s, 9, 10, 34, 92, s);%2$c}%2$c%2$cint%1$cmain(void)%2$c{%2$c%1$c// The string below contain the program source code%2$c%1$cchar%1$c*s = %3$c%5$s%3$c;%2$c%2$c%1$cprint(s);%2$c%1$creturn (0);%2$c}%2$c";
 
 	print(s);
 	return (0);
